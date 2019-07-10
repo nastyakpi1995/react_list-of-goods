86636663014432d77637e3ef5bfefca8dfacaaa6
Nastia Krat <nastyakpi1995@gmail.com>
2019-07-10 08:11:17 +0300

    Updates

---
+++ precache-manifest.f0b83d64926031ceed10b8ec4de7d443.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcef46bf35e03889817251b73189988e",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_list-of-goods/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4bc28f39d1a9a89c6e94",
+    "url": "/react_list-of-goods/static/js/main.001ef58d.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.f0b83d64926031ceed10b8ec4de7d443.js"
 );
 
 self.addEventListener('message', (event) => {
